40ae7b289c7f08654c6c10936a148518fd9c1073
crapas <crapas@gmail.com>
2023-07-14 10:47:23 +0900

    Test

--- infer/infer_test.py
+++ infer/infer_test.py
@@ -23,12 +23,15 @@ def select_one_jpg(number):
         return None
     
 class TestInfer(unittest.TestCase):
+
     # paraeterized decorator로 하나의 함수로 가능한 여러 테스트를 한 번의 정의로 가능합니다.
     @parameterized.expand([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     def test_infer(self, number):
         _, result = infer(select_one_jpg(number))
         self.assertEqual(result, number)
- 
+
+
+
 if __name__ == "__main__":
     success_threshold = 8
     suite = unittest.TestLoader().loadTestsFromTestCase(TestInfer)
